353e992d5be417043d73c8864988edbe8b676600
blade <blade.barringer@getbraintree.com>
2018-08-11 22:17:53 -0500

    Add additional warning

--- src/pages/deck-builder.vue
+++ src/pages/deck-builder.vue
@@ -153,6 +153,7 @@
         <ul class="has-text-left">
           <li>Your deck will be saved automatically when you make a change, but it is only saved locally to your browser's local storage. There is no database persisting your deck. So if you clear your browser's data, you'll lose your deck.</li>
           <li>Currently, only one deck can be saved a time.</li>
+          <li>As changes are made to the app, your previously saved deck may become incompatible and be lost. Do not rely on this deck builder until it goes out of Beta.</li>
           <li>This is very much a work in progress project, so don't be surprised if there are bugs. If you find one, <a class="is-link has-text-blue" href="https://github.com/crookedneighbor/mtg-deck-builder-demo/issues">kindly report it on Github</a>.</li>
         </ul>
       </div>
