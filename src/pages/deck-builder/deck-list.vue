17fa703a20388fd3ac83648731256f3bc7252ea2
blade <blade.barringer@getbraintree.com>
2018-08-24 23:09:53 -0500

    Clean up deck list module

--- src/pages/deck-builder/deck-list.vue
+++ src/pages/deck-builder/deck-list.vue
@@ -2,16 +2,23 @@
   <div>
     <div id="deck-selection" class="tabs is-centered is-boxed">
       <ul>
-        <li v-for="(obj, type) in listTypes" :class="{'is-active': type === deckView}" @click="selectType(type)" v-if="obj.shouldShow()">
+        <li
+          v-for="list in listTypeOptions" :key="`card-list-selection-${list.key}`"
+          :class="{'is-active': list.key === deckView}"
+          @click="selectType(list.key)"
+        >
           <a>
-            <span>{{obj.name}}</span>
+            <span>{{list.name}}</span>
           </a>
         </li>
       </ul>
     </div>
 
     <div class="box">
-      <card-list v-for="(obj, type) in listTypes" :type="type"></card-list>
+      <card-list
+        v-for="list in listTypes" :key="`card-list-${list.key}`"
+        :type="list.key"
+        ></card-list>
     </div>
   </div>
 </template>
@@ -19,7 +26,6 @@
 <script>
 const {mapState, mapGetters} = require('vuex')
 const CardList = require('./card-list.vue')
-const DECK_LIST_TYPES = require('../../lib/constants').DECK_LIST_TYPES
 
 export default {
   components: {
@@ -27,57 +33,57 @@ export default {
   },
   data () {
     return {
-      listTypes: {
-        mainDeck: {
-          name: 'Main Deck',
-          shouldShow () {
-            return true
-          }
-        },
-        commandZone: {
-          name: 'Command Zone',
-          shouldShow: () => {
-            return this.hasCommandZone
-          }
-        },
-        sideboard: {
-          name: 'Sideboard',
-          shouldShow () {
-            return true
-          }
+      listTypes: [{
+        key: 'mainDeck',
+        name: 'Main Deck',
+        shouldShow () {
+          return true
         }
-      }
+      }, {
+        key: 'commandZone',
+        name: 'Command Zone',
+        shouldShow: () => {
+          return this.hasCommandZone
+        }
+      }, {
+        key: 'sideboard',
+        name: 'Sideboard',
+        shouldShow () {
+          return true
+        }
+      }]
     }
   },
   methods: {
     selectType (type) {
       this.$store.commit('updateDeckView', type)
-    },
-    incrementDeckView (index) {
     }
   },
   computed: Object.assign(
     mapState([
       'deckView'
     ]),
-    mapGetters(['hasCommandZone'])
+    mapGetters(['hasCommandZone']),
+    {
+      listTypeOptions () {
+        return this.listTypes.filter((list) => {
+          return list.shouldShow()
+        })
+      }
+    }
   ),
   created () {
     this.$root.$on('toggle-deck-view', () => {
-      let currentIndex = DECK_LIST_TYPES.indexOf(this.deckView)
-      let nextIndex = currentIndex + 1
-      let view = this.listTypes[DECK_LIST_TYPES[nextIndex]]
+      let currentIndex = this.listTypeOptions.findIndex((list) => {
+        return list.key === this.deckView
+      })
+      let view = this.listTypeOptions[currentIndex + 1]
 
-      while (view && !view.shouldShow()) {
-        nextIndex++
-        view = this.listTypes[DECK_LIST_TYPES[nextIndex]]
+      if (!view) {
+        view = this.listTypeOptions[0]
       }
 
-      if (!DECK_LIST_TYPES[nextIndex]) {
-        nextIndex = 0
-      }
-
-      this.$store.commit('updateDeckView', DECK_LIST_TYPES[nextIndex])
+      this.selectType(view.key)
     })
   }
 }
