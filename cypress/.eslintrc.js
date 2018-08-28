bc543398d199660118516278a27df5c723939879
blade <blade.barringer@getbraintree.com>
2018-08-27 23:01:04 -0500

    Lint integration tests

---
+++ cypress/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  globals: {
+    Cypress: false,
+    cy: false,
+    it: false,
+    describe: false,
+    after: false,
+    before: false,
+    afterEach: false,
+    beforeEach: false
+  }
+}
